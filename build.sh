0646e2be7ad8d136c06a5c6ad97fc840eeef7667
tkardi <tonis.kardi@gmail.com>
2022-01-23 10:26:01 +0200

    fix build path name + add gitignore (#4)

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-rm -r ./kartogrmm-build
-mkdir ./kartogrmm-build
+rm -r ./kartogramm-build
+mkdir ./kartogramm-build
 
-cp -r ./data ./kartogrmm-build/data
-cp -r ./db ./kartogrmm-build/db
-cp -r ./requirements.txt ./kartogrmm-build/requirements.txt
-cp -r ./docker/** ./kartogrmm-build
+cp -r ./data ./kartogramm-build/data
+cp -r ./db ./kartogramm-build/db
+cp -r ./requirements.txt ./kartogramm-build/requirements.txt
+cp -r ./docker/** ./kartogramm-build
 
 cd kartogrmm-build
 
