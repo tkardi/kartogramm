b9a0a7921a2bec4f13561562e320e7434b36f2f0
tkardi <tonis.kardi@gmail.com>
2022-02-03 18:41:42 +0200

    repair build.sh (#5)

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -r ./kartogramm-build
+rm -rf ./kartogramm-build || true
 mkdir ./kartogramm-build
 
 cp -r ./data ./kartogramm-build/data
@@ -8,6 +8,6 @@ cp -r ./db ./kartogramm-build/db
 cp -r ./requirements.txt ./kartogramm-build/requirements.txt
 cp -r ./docker/** ./kartogramm-build
 
-cd kartogrmm-build
+cd kartogramm-build
 
 docker build --tag localhost/kartogramm -f Dockerfile .
